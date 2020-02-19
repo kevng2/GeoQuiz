6c5dff4543d32b684992d19c223deaddc3b0805a
Kevin Nguyen <nguyenkt6123@gmail.com>
2020-02-18 20:50:13 -0500

    added more questions and a next button

--- app/src/main/java/com/example/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -4,15 +4,35 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+    private int mCurrentIndex = 0;
+    private Question[] mQuestionBank = new Question[]{
+        new Question(R.string.question_australia, true),
+        new Question(R.string.question_ocean, true),
+        new Question(R.string.question_mideast, false),
+        new Question(R.string.question_africa, false),
+        new Question(R.string.question_americas, true),
+        new Question(R.string.question_asia, true),
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initializes the text on the screen with a value
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+
+        // gets the current question
+        // sets the text into the test view
+        updateQuestion();
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -25,17 +45,45 @@ public class MainActivity extends AppCompatActivity {
                  */
 
                 //  Toast.show() displays it on the screen
-                Toast.makeText(MainActivity.this, R.string.correct_toast,
-                                Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
+
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, R.string.incorrect_toast,
-                                Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
+
+        // creating listener for next button
+        // finding button from unique name given in xml
+        mNextButton =  (Button) findViewById(R.id.next_button);
+
+        mNextButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // increment index and reset to 0 again if bank reaches max index
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+    }
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        int messageResId = 0;
+        if(answerIsTrue == userPressedTrue) {
+            messageResId = R.string.correct_toast;
+        }
+        else
+            messageResId = R.string.incorrect_toast;
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/example/geoquiz/Question.java
@@ -0,0 +1,29 @@
+package com.example.geoquiz;
+
+public class Question {
+    // question text and question answer
+    // mTextResId is an int because it will hold the resource ID (always an int) of a string
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
